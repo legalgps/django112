67ddfe9a147bf1a0388dcae26ad38830f8ef1d5b
Chris Daming <chris@legalgps.com>
2021-09-20 21:45:18 -0500

    FSDI 112 class 1

---
+++ pages/tests.py
@@ -0,0 +1,27 @@
+from django.test import SimpleTestCase
+
+
+class SimpleTests(SimpleTestCase):
+    def test_home_page_status_code(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_about_page_status_code(self):
+        response = self.client.get("/about/")
+        self.assertEqual(response.status_code, 200)
+    
+    def test_home_page_uses_correct_template(self):
+        response = self.client.get("/")
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_about_page_uses_correct_template(self):
+        response = self.client.get("/about/")
+        self.assertTemplateUsed(response, 'about.html')
+
+    def test_home_page_extends_base_template(self):
+        response = self.client.get("/")
+        self.assertTemplateUsed(response, 'base.html')
+
+    def test_about_page_extends_base_template(self):
+        response = self.client.get("/about/")
+        self.assertTemplateUsed(response, 'base.html')
